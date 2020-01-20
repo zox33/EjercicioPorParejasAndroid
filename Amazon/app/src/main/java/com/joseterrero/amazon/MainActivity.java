e66b58e6f9e190e87ab29a3af1e18de0ccb372c1
jmterrero <josetg999@gmail.com>
2020-01-20 08:22:11 +0100

    amazon

---
+++ Amazon/app/src/main/java/com/joseterrero/amazon/MainActivity.java
@@ -0,0 +1,14 @@
+package com.joseterrero.amazon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
